1742cba11bba12e6407a2e1d41485b3ed76d4af9
St4r_Command <romain.hunault@gmail.com>
2015-03-08 19:22:03 +0100

    Deleted room bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new MCDH\HotelBundle\MCDHHotelBundle(),
-            new MCDH\RoomBundle\MCDHRoomBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/MCDH/RoomBundle/MCDHRoomBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MCDH\RoomBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MCDHRoomBundle extends Bundle
-{
-}
