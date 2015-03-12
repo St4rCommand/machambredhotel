6e5135ed18cf3184673f78dd5d0eabf09b7de30e
St4r_Command <romain.hunault@gmail.com>
2015-03-12 23:25:00 +0100

    created booking form

---
+++ src/MCDH/HotelBundle/Form/BookingType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MCDH\HotelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class BookingType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('beginDate',	'date')
+            ->add('endDate',	'date')
+            ->add('price',		'money')
+            ->add('people',		'integer')
+            ->add('breakfast',	'boolean')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MCDH\HotelBundle\Entity\Booking'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mcdh_hotelbundle_booking';
+    }
+}
