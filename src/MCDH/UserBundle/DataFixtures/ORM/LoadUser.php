e051c504709fcdbf88b4c0ee1c3a37e4b159c4b3
St4r_Command <romain.hunault@gmail.com>
2015-03-19 01:19:22 +0100

    add fixtures

--- src/MCDH/UserBundle/DataFixtures/ORM/LoadUser.php
+++ src/MCDH/UserBundle/DataFixtures/ORM/LoadUser.php
@@ -18,6 +18,24 @@ class LoadUser implements FixtureInterface{
 		$adminUser->setRoles(array('ROLE_ADMIN'));
 		$manager->persist($adminUser);
 	    $manager->flush();
+	    
+	    $customerUser = new User;
+	    $customerUser->setUserName('customer');
+	    $customerUser->setPlainPassword('customer');
+	    $customerUser->setEmail('customer@mail.com');
+	    $customerUser->setEnabled(true);
+	    $customerUser->setRoles(array('ROLE_CUSTOMER'));
+	    $manager->persist($customerUser);
+	    $manager->flush();
+	    
+	    $hotelKeeperUser = new User;
+	    $hotelKeeperUser->setUserName('hotelkeeper');
+	    $hotelKeeperUser->setPlainPassword('hotelkeeper');
+	    $hotelKeeperUser->setEmail('hotelkeeper@mail.com');
+	    $hotelKeeperUser->setEnabled(true);
+	    $hotelKeeperUser->setRoles(array('ROLE_HOTELKEEPER'));
+	    $manager->persist($hotelKeeperUser);
+	    $manager->flush();
 
   }
 
